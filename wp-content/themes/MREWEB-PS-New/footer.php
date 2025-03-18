b3e0b0aae486843f47ed8449b2d44faae968eb33
mohamadreza esmaeeli <50191824+Mreweb@users.noreply.github.com>
2025-03-18 07:29:34 +0330

    Update Footer

--- wp-content/themes/MREWEB-PS-New/footer.php
+++ wp-content/themes/MREWEB-PS-New/footer.php
@@ -1,78 +1,134 @@
-<?php global $_URL, $_DIR, $Main_URL; ?>
+<?php global $_URL, $_DIR, $MAIN_URL; ?>
 
 
 <!-- footer -->
 <footer class="footer">
     <div class="top-footer">
         <div class="container">
+            <style>
+                .footer .top-footer .content-footer-top .description-logo {
+                    display: flex;
+                    align-items: center;
+                    gap: 40px;
+                    flex-direction: row;
+                }
+
+                .footer .top-footer .content-footer-top .description {
+                    color: #a3abb0;
+                    line-height: 2;
+                    flex-grow: 1;
+                    text-align: justify;
+                    font-size: 14px;
+                }
+
+                .footer .wd-social {
+                    display: flex;
+                    align-items: center;
+                    justify-content: end;
+                }
+            </style>
             <div class="content-footer-top">
-                <div class="footer-logo">
-                    <a href="<?php echo $_URL; ?>">
-                        <img alt="logo" src="<?php echo $_DIR; ?>images/logo/logofont.png" width="166" />
-                    </a>
+                <div class="col-12 description-logo ">
+                    <div class="footer-logo">
+                        <a href="<?php echo $MAIN_URL; ?>">
+                            <img alt="logo" src="<?php echo $_DIR; ?>images/logo/logofont.png"/>
+                        </a>
+                    </div>
+                    <p class="description">پلتفرم <span class="fw-bold">کرادیاب</span> به عنوان مرجع تخصصی تأمین مالی
+                        جمعی، فرصتی نوین برای سرمایه گذاران و کسب وکارها فراهم آورده است تا بهترین مسیرهای سرمایه گذاری
+                        و جذب سرمایه را شناسایی کنند. در بخش<!-- --> <span class="fw-bold">کرادیاب</span>، کاربران می
+                        توانند طرح های تأمین مالی را مشاهده، مقایسه و تحلیل کرده و با بهره گیری از سبدهای پیشنهادی مبتنی
+                        بر هوش مصنوعی، تصمیمات سرمایه گذاری هوشمندانه تری اتخاذ کنند.<!-- --> <span class="fw-bold">کرادکاو</span>
+                        با ارائه گزارش های جامع آماری و تحلیلی، تصویری دقیق از وضعیت تأمین مالی جمعی در کشور ترسیم می
+                        کند. همچنین،<!-- --> <span class="fw-bold">خدمات شرکتی</span> این مجموعه با ارائه مشاوره های
+                        تخصصی در زمینه تأمین مالی شرکت ها، تأسیس سکوهای تأمین مالی جمعی، اصلاح ساختار شرکتی و پذیرش در
+                        بازار سرمایه، راهکارهایی کارآمد برای رشد و توسعه کسب وکارها ارائه می دهد.</p>
                 </div>
-                <div class="wd-social">
-                    <span>ما را دنبال کنید:</span>
+                <div class="wd-social col-12"><span>ما را دنبال کنید:</span>
                     <ul class="list-social d-flex align-items-center">
-                        <li>
-                            <a class="box-icon w-40 social" href="https://instagram.com/crowdyab">
-                                <svg
-                                        fill="none"
-                                        height="14"
-                                        viewbox="0 0 14 14"
-                                        width="14"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                            d="M6.99812 4.66567C5.71277 4.66567 4.66383 5.71463 4.66383 7C4.66383 8.28537 5.71277 9.33433 6.99812 9.33433C8.28346 9.33433 9.3324 8.28537 9.3324 7C9.3324 5.71463 8.28346 4.66567 6.99812 4.66567ZM13.9992 7C13.9992 6.03335 14.008 5.07545 13.9537 4.11055C13.8994 2.98979 13.6437 1.99512 12.8242 1.17556C12.0029 0.35426 11.01 0.100338 9.88927 0.0460516C8.92263 -0.00823506 7.96475 0.000520879 6.99987 0.000520879C6.03323 0.000520879 5.07536 -0.00823506 4.11047 0.0460516C2.98973 0.100338 1.99508 0.356011 1.17554 1.17556C0.354253 1.99687 0.100336 2.98979 0.0460508 4.11055C-0.00823491 5.0772 0.00052087 6.0351 0.00052087 7C0.00052087 7.9649 -0.00823491 8.92455 0.0460508 9.88945C0.100336 11.0102 0.356004 12.0049 1.17554 12.8244C1.99683 13.6457 2.98973 13.8997 4.11047 13.9539C5.07711 14.0082 6.03499 13.9995 6.99987 13.9995C7.9665 13.9995 8.92438 14.0082 9.88927 13.9539C11.01 13.8997 12.0047 13.644 12.8242 12.8244C13.6455 12.0031 13.8994 11.0102 13.9537 9.88945C14.0097 8.92455 13.9992 7.96665 13.9992 7ZM6.99812 10.5917C5.01056 10.5917 3.40651 8.98759 3.40651 7C3.40651 5.01241 5.01056 3.40832 6.99812 3.40832C8.98567 3.40832 10.5897 5.01241 10.5897 7C10.5897 8.98759 8.98567 10.5917 6.99812 10.5917ZM10.7368 4.10004C10.2728 4.10004 9.89802 3.72529 9.89802 3.26122C9.89802 2.79716 10.2728 2.42241 10.7368 2.42241C11.2009 2.42241 11.5756 2.79716 11.5756 3.26122C11.5758 3.37142 11.5542 3.48056 11.5121 3.58239C11.47 3.68422 11.4082 3.77675 11.3303 3.85467C11.2523 3.93258 11.1598 3.99437 11.058 4.03647C10.9562 4.07858 10.847 4.10018 10.7368 4.10004Z"
-                                            fill="white"
-                                    ></path>
+                        <li><a class="box-icon w-40 social" href="https://www.facebook.com/crowdyab"
+                               rel="noopener noreferrer" target="_blank">
+                                <svg class="icon" fill="none" height="16" viewBox="0 0 9 16" width="9"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7.60547 9L8.00541 6.10437H5.50481V4.22531C5.50481 3.43313 5.85413 2.66094 6.97406 2.66094H8.11087V0.195625C8.11087 0.195625 7.07925 0 6.09291 0C4.03359 0 2.68753 1.38688 2.68753 3.8975V6.10437H0.398438V9H2.68753V16H5.50481V9H7.60547Z"
+                                          fill="white"></path>
                                 </svg>
-                            </a>
-                        </li>
+                            </a></li>
+                        <li><a class="box-icon w-40 social" href="https://www.linkedin.com/in/crowdyab"
+                               rel="noopener noreferrer" target="_blank">
+                                <svg fill="none" height="16" viewBox="0 0 16 16" width="16"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3.58151 16H0.264292V5.31762H3.58151V16ZM1.92111 3.86044C0.860376 3.86044 0 2.98185 0 1.92111C7.59231e-09 1.4116 0.140303 0.92296 0.562681 0.562681C0.92296 0.140303 1.4116 0 1.92111 0C2.43063 0 2.91927 0.140303 3.27955 0.562681C3.63983 0.92296 3.84223 1.4116 3.84223 1.92111C3.84223 2.98185 2.98149 3.86044 1.92111 3.86044ZM15.9968 16H12.6867V10.7999C12.6867 9.56057 12.6617 7.97125 10.962 7.97125C9.23735 7.97125 8.97306 9.31771 8.97306 10.7106V16H5.65941V5.31762H8.84091V6.77479H8.88734C9.33021 5.93549 10.412 5.04976 12.026 5.04976C15.3832 5.04976 16.0004 7.26052 16.0004 10.132V16H15.9968Z"
+                                          fill="white"></path>
+                                </svg>
+                            </a></li>
+                        <li><a class="box-icon w-40 social" href="https://x.com/crowdyab" rel="noopener noreferrer"
+                               target="_blank">
+                                <svg fill="none" height="14" viewBox="0 0 14 14" width="14"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.30314 5.92804L13.4029 0H12.1944L7.7663 5.14724L4.22958 0H0.150391L5.4986 7.78354L0.150391 14H1.35894L6.03514 8.56434L9.77017 14H13.8494L8.30284 5.92804H8.30314ZM6.64787 7.85211L6.10598 7.07705L1.79439 0.909771H3.65065L7.13015 5.88696L7.67204 6.66202L12.195 13.1316H10.3387L6.64787 7.85241V7.85211Z"
+                                          fill="white"></path>
+                                </svg>
+                            </a></li>
+                        <li><a class="box-icon w-40 social" href="https://www.instagram.com/crowdyab"
+                               rel="noopener noreferrer" target="_blank">
+                                <svg fill="none" height="14" viewBox="0 0 14 14" width="14"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6.99812 4.66567C5.71277 4.66567 4.66383 5.71463 4.66383 7C4.66383 8.28537 5.71277 9.33433 6.99812 9.33433C8.28346 9.33433 9.3324 8.28537 9.3324 7C9.3324 5.71463 8.28346 4.66567 6.99812 4.66567ZM13.9992 7C13.9992 6.03335 14.008 5.07545 13.9537 4.11055C13.8994 2.98979 13.6437 1.99512 12.8242 1.17556C12.0029 0.35426 11.01 0.100338 9.88927 0.0460516C8.92263 -0.00823506 7.96475 0.000520879 6.99987 0.000520879C6.03323 0.000520879 5.07536 -0.00823506 4.11047 0.0460516C2.98973 0.100338 1.99508 0.356011 1.17554 1.17556C0.354253 1.99687 0.100336 2.98979 0.0460508 4.11055C-0.00823491 5.0772 0.00052087 6.0351 0.00052087 7C0.00052087 7.9649 -0.00823491 8.92455 0.0460508 9.88945C0.100336 11.0102 0.356004 12.0049 1.17554 12.8244C1.99683 13.6457 2.98973 13.8997 4.11047 13.9539C5.07711 14.0082 6.03499 13.9995 6.99987 13.9995C7.9665 13.9995 8.92438 14.0082 9.88927 13.9539C11.01 13.8997 12.0047 13.644 12.8242 12.8244C13.6455 12.0031 13.8994 11.0102 13.9537 9.88945C14.0097 8.92455 13.9992 7.96665 13.9992 7ZM6.99812 10.5917C5.01056 10.5917 3.40651 8.98759 3.40651 7C3.40651 5.01241 5.01056 3.40832 6.99812 3.40832C8.98567 3.40832 10.5897 5.01241 10.5897 7C10.5897 8.98759 8.98567 10.5917 6.99812 10.5917ZM10.7368 4.10004C10.2728 4.10004 9.89802 3.72529 9.89802 3.26122C9.89802 2.79716 10.2728 2.42241 10.7368 2.42241C11.2009 2.42241 11.5756 2.79716 11.5756 3.26122C11.5758 3.37142 11.5542 3.48056 11.5121 3.58239C11.47 3.68422 11.4082 3.77675 11.3303 3.85467C11.2523 3.93258 11.1598 3.99437 11.058 4.03647C10.9562 4.07858 10.847 4.10018 10.7368 4.10004Z"
+                                          fill="white"></path>
+                                </svg>
+                            </a></li>
+                        <li><a class="box-icon w-40 social" href="https://t.me/crowdyab_ir" rel="noopener noreferrer"
+                               target="_blank">
+                                <svg height="20px" version="1.1" viewBox="0 0 24 24" width="20px"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <g fill="none" fill-rule="evenodd" stroke="none" stroke-width="1">
+                                        <g id="Brand" transform="translate(-672.000000, -48.000000)">
+                                            <g id="telegram_fill" transform="translate(672.000000, 48.000000)">
+                                                <path d="M24,0 L24,24 L0,24 L0,0 L24,0 Z M12.5934901,23.257841 L12.5819402,23.2595131 L12.5108777,23.2950439 L12.4918791,23.2987469 L12.4918791,23.2987469 L12.4767152,23.2950439 L12.4056548,23.2595131 C12.3958229,23.2563662 12.3870493,23.2590235 12.3821421,23.2649074 L12.3780323,23.275831 L12.360941,23.7031097 L12.3658947,23.7234994 L12.3769048,23.7357139 L12.4804777,23.8096931 L12.4953491,23.8136134 L12.4953491,23.8136134 L12.5071152,23.8096931 L12.6106902,23.7357139 L12.6232938,23.7196733 L12.6232938,23.7196733 L12.6266527,23.7031097 L12.609561,23.275831 C12.6075724,23.2657013 12.6010112,23.2592993 12.5934901,23.257841 L12.5934901,23.257841 Z M12.8583906,23.1452862 L12.8445485,23.1473072 L12.6598443,23.2396597 L12.6498822,23.2499052 L12.6498822,23.2499052 L12.6471943,23.2611114 L12.6650943,23.6906389 L12.6699349,23.7034178 L12.6699349,23.7034178 L12.678386,23.7104931 L12.8793402,23.8032389 C12.8914285,23.8068999 12.9022333,23.8029875 12.9078286,23.7952264 L12.9118235,23.7811639 L12.8776777,23.1665331 C12.8752882,23.1545897 12.8674102,23.1470016 12.8583906,23.1452862 L12.8583906,23.1452862 Z M12.1430473,23.1473072 C12.1332178,23.1423925 12.1221763,23.1452606 12.1156365,23.1525954 L12.1099173,23.1665331 L12.0757714,23.7811639 C12.0751323,23.7926639 12.0828099,23.8018602 12.0926481,23.8045676 L12.108256,23.8032389 L12.3092106,23.7104931 L12.3186497,23.7024347 L12.3186497,23.7024347 L12.3225043,23.6906389 L12.340401,23.2611114 L12.337245,23.2485176 L12.337245,23.2485176 L12.3277531,23.2396597 L12.1430473,23.1473072 Z"
+                                                      fill-rule="nonzero" id="MingCute"></path>
+                                                <path d="M19.7773,4.42984 C20.8652,3.97177 22.0315,4.8917 21.8394,6.05639 L19.5705,19.8131 C19.3517,21.1395 17.8949,21.9006 16.678,21.2396 C15.6597,20.6865 14.1489,19.8352 12.7873,18.9455 C12.1074,18.5012 10.0255,17.0766 10.2814,16.0625 C10.5002,15.1954 14.0001,11.9375 16.0001,10 C16.7857,9.23893 16.4279,8.79926 15.5001,9.5 C13.1985,11.2383 9.50332,13.8812 8.28136,14.625 C7.20323,15.2812 6.64031,15.3932 5.96886,15.2812 C4.74273,15.0769 3.60596,14.7605 2.67788,14.3758 C1.42351,13.8558 1.48461,12.132 2.67703,11.63 L19.7773,4.42984 Z"
+                                                      fill="#fff"></path>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </a></li>
                     </ul>
                 </div>
+
             </div>
         </div>
     </div>
     <div class="inner-footer">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-6">
                     <div class="footer-cl-1">
                         <ul class="mt-12">
-                            <li class="mt-12 d-flex align-items-center gap-8">
-                                <i class="icon icon-mapPinLine fs-20 text-variant-2"></i>
-                                <p class="text-white">
-                                    تهران میرداماد قبل از خیابان نیل جنب ستاد مرکزی بیمه دی ساختمان سبز(۲۳۷) طبقه ۴ واحد بیست
-                                </p>
-                            </li>
-                            <li class="mt-12 d-flex align-items-center gap-8">
-                                <i class="icon icon-phone2 fs-20 text-variant-2"></i>
-                                <a class="text-white caption-1" href="tel:02122255095">
-                                    02122255095
-                                </a>
-                            </li>
-                            <li class="mt-12 d-flex align-items-center gap-8">
-                                <i class="icon icon-phone2 fs-20 text-variant-2"></i>
-                                <a class="text-white caption-1" href="#">
-                                    1918955177
-                                </a>
-                            </li>
-                            <li class="mt-12 d-flex align-items-center gap-8">
-                                <i class="icon icon-mail fs-20 text-variant-2"></i>
-                                <p class="text-white">crowdyab.ir@gmail.com</p>
+                            <li class="mt-12 d-flex align-items-center gap-8"><i
+                                        class="icon icon-mapPinLine fs-20 text-variant-2"></i>
+                                <p class="text-white">تهران، میرداماد، قبل از خیابان نیل، جنب ستاد مرکزی بیمه دی،
+                                    ساختمان سبز (۲۳۷)، طبقه ۴، واحد ۲۰</p></li>
+                            <li class="mt-12 d-flex align-items-center gap-8"><i
+                                        class="icon icon-mapPinLine fs-20 text-variant-2"></i><span
+                                        class="text-white caption-1">کد پستی: 19189551۷۷</span></li>
+                            <li class="mt-12 d-flex align-items-center gap-8"><i
+                                        class="icon icon-phone2 fs-20 text-variant-2"></i><a
+                                        class="text-white caption-1" href="tel:02122255095">021-22255095</a></li>
+                            <li class="mt-12 d-flex align-items-center gap-8"><i
+                                        class="icon icon-mail fs-20 text-variant-2"></i><a class="text-white"
+                                                                                           href="mailto:crowdyab.ir@gmail.com">crowdyab.ir@gmail.com</a>
                             </li>
                         </ul>
                     </div>
                 </div>
-
                 <div class="col-lg-3 col-md-6">
                     <div class="footer-cl-2 footer-col-block">
                         <div class="fw-7 text-white footer-heading-mobile">دسته بندی ها</div>
-                        <div class="tf-collapse-content">
+                        <div class="">
                             <ul class="mt-10 navigation-menu-footer">
-                                <li><a class="caption-1 text-variant-2" href="https://crowdyab.ir/plans">فرصت های سرمایه گزاری</a></li>
+                                <li><a class="caption-1 text-variant-2" href="<?php echo $MAIN_URL; ?>/plans">فرصت های سرمایه گذاری</a></li>
                                 <li><a class="caption-1 text-variant-2" href="https://blog.crowdyab.ir/تماس-با-ما/">درخواست
                                         تامین مالی</a></li>
                                 <li><a class="caption-1 text-variant-2" href="#">باشگاه سرمایه گذاران</a></li>
@@ -83,12 +139,14 @@
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <div class="footer-cl-3 footer-col-block">
-                        <div class="fw-7 text-white footer-heading-mobile">شرکت ما</div>
-                        <div class="tf-collapse-content">
+                        <div class="fw-7 text-white footer-heading-mobile">خدمات ما</div>
+                        <div class="">
                             <ul class="mt-10 navigation-menu-footer">
-                                <li><a class="caption-1 text-variant-2" href="#">بخش ها</a></li>
-                                <li><a class="caption-1 text-variant-2" href="#">همکاران</a></li>
-                                <li><a class="caption-1 text-variant-2" href="#">هیئت مدیره</a></li>
+                                <li><a class="caption-1 text-variant-2" href=" https://panel.crowdyab.ir/auth/login/">کرادیاب</a>
+                                </li>
+                                <li><a class="caption-1 text-variant-2"
+                                       href="https://blog.crowdyab.ir/کرادکاو/">کرادکاو</a></li>
+                                <li><a class="caption-1 text-variant-2" href="<?php echo $_URL; ?>خدمات-شرکتی">خدمات شرکتی</a></li>
                             </ul>
                         </div>
                     </div>
@@ -103,11 +161,11 @@
                 <div class="copyright">
                     ©1404 CrowdYab. تمامی حقوق محفوظ است.
                 </div>
-<!--                <ul class="menu-bottom">-->
-<!--                    <li><a href="our-service.html">شرایط خدمات</a></li>-->
-<!--                    <li><a href="pricing.html">سیاست حفظ حریم</a></li>-->
-<!--                    <li><a href="contact.html">خط مشی کوکی</a></li>-->
-<!--                </ul>-->
+                <!--                <ul class="menu-bottom">-->
+                <!--                    <li><a href="our-service.html">شرایط خدمات</a></li>-->
+                <!--                    <li><a href="pricing.html">سیاست حفظ حریم</a></li>-->
+                <!--                    <li><a href="contact.html">خط مشی کوکی</a></li>-->
+                <!--                </ul>-->
             </div>
         </div>
     </div>
