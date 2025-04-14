d5656871420b9d7056a39578d6c42999db77ace1
mohamadreza esmaeeli <50191824+Mreweb@users.noreply.github.com>
2025-04-14 10:39:49 +0330

    update footer

--- wp-content/themes/MREWEB-PS-New/footer-new.php
+++ wp-content/themes/MREWEB-PS-New/footer-new.php
@@ -58,9 +58,8 @@
                         <div class="fw-7 text-white footer-heading-mobile">دسته بندی ها</div>
                         <div class="tf-collapse-content">
                             <ul class="mt-10 navigation-menu-footer">
-                                <li><a class="caption-1 text-variant-2" href="/plans">فرصت های سرمایه گزاری</a></li>
-                                <li><a class="caption-1 text-variant-2" href="https://blog.crowdyab.ir/تماس-با-ما/">درخواست
-                                        تامین مالی</a></li>
+                                <li><a class="caption-1 text-variant-2" href="https://crowdyab.ir/plans">فرصت های سرمایه گزاری</a></li>
+                                 <li><a class="caption-1 text-variant-2" href="https://blog.crowdyab.ir/سوالات-متداول">سوالات متداول</a></li>
                                 <li><a class="caption-1 text-variant-2" href="#">باشگاه سرمایه گذاران</a></li>
                                 <li><a class="caption-1 text-variant-2" href="https://blog.crowdyab.ir/">وبلاگ</a></li>
                             </ul>
