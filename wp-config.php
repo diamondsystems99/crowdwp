4d95e9c1c005b2ebb8559f5d9af5a6df8a8d6dff
mohamadreza esmaeeli <50191824+Mreweb@users.noreply.github.com>
2025-05-03 15:17:09 +0330

    update config.php

--- wp-config.php
+++ wp-config.php
@@ -26,13 +26,13 @@ define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settin
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'persian_crud_blog' );
+define( 'DB_NAME', 'wordpress_blog' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'blogroot' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '2SyzW%qh1OU57wCEo6Y8' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
